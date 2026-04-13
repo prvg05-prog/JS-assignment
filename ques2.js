9862dee10c64a95b4e5292e04967286e16f8763b
prvg05-prog <prvg05@gmail.com>
2026-04-14 00:23:38 +0530

    ques2.js

--- ques2.js
+++ ques2.js
@@ -10,7 +10,7 @@ function inc(x){
 
 function checkmid(x,seed){
     var mid=Math.floor(x / 10) % 10;
-    if(mid===seed){
+    if(mid==seed){
         return 1;
     }
     return 0;
@@ -20,7 +20,7 @@ function checkmid(x,seed){
 let num= prompt("enter number");
 let seed= prompt("enter seed");
 for(var i=0;i<3;i++){
-    if(num % 2===0){
+    if(num % 2==0){
         num= num/2 +seed;
     }
     else{
@@ -30,7 +30,7 @@ for(var i=0;i<3;i++){
 
 var incs=inc(num);
 var smid=checkmid(num,seed);
-if(incs===smid && smid ===1){
+if(incs==smid && smid ==1){
     alert("yes");
 }
 
